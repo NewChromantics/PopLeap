dbdf899f07695b3753f850c9a047c4e6103aa0a2
Graham Reeves <graham@grahamreeves.com>
2016-03-13 14:06:27 +0000

    Initial minimal build for osx

---
+++ src/PopLeap.cpp
@@ -0,0 +1,19 @@
+#include "PopLeap.h"
+#include <iostream>
+
+
+int main()
+{
+	TPopLeapApp App;
+
+	
+	std::cout << "hello!" << std::endl;
+	
+	App.mConsoleApp.WaitForExit();
+
+	return 0;
+}
+
+
+
+
---
+++ src/PopLeap.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+#include <SoyApp.h>
+
+
+
+class TPopLeapApp
+{
+public:
+	Soy::Platform::TConsoleApp	mConsoleApp;
+};
+
+
