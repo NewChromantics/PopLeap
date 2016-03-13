dca0a3af28513bcaae63c728f27c4a42be94c19c
Graham Reeves <graham@grahamreeves.com>
2016-03-13 18:02:56 +0000

    Initial simple http server

--- src/PopLeap.cpp
+++ src/PopLeap.cpp
@@ -1,13 +1,82 @@
 #include "PopLeap.h"
-#include <iostream>
+#include <SoyHttpServer.h>
+
+
+
+namespace PopLeap
+{
+	namespace Private
+	{
+		std::mutex		DebugLogLock;
+		std::string		DebugLog;
+	}
+	
+	std::string			GetDebugLog();
+	void				PushDebugLog(const std::string& String);
+}
+
+
+
+std::string PopLeap::GetDebugLog()
+{
+	std::lock_guard<std::mutex> Lock( Private::DebugLogLock );
+	return Private::DebugLog;
+}
+
+void PopLeap::PushDebugLog(const std::string& String)
+{
+	std::lock_guard<std::mutex> Lock( Private::DebugLogLock );
+	Private::DebugLog += String;
+}
+
+
+
+void OnHttpRequest(const Http::TRequestProtocol& Request,SoyRef Client,THttpServer& HttpServer)
+{
+	std::Debug << "request for " << Request.mUrl << " from " << Client << std::endl;
+
+	if ( Request.mUrl.empty() )
+	{
+		Http::TResponseProtocol Response;
+		Response.SetContent("hello! Try /debug and /ping");
+		HttpServer.SendResponse( Response, Client );
+		return;
+	}
+	
+	if ( Request.mUrl == "debug" )
+	{
+		Http::TResponseProtocol Response;
+		auto DebugLog = PopLeap::GetDebugLog();
+		Response.SetContent( DebugLog );
+		HttpServer.SendResponse( Response, Client );
+		return;
+	}
+	
+	if ( Request.mUrl == "ping" )
+	{
+		Http::TResponseProtocol Response;
+		Response.SetContent( GetArrayBridge(Request.mContent), Request.mContentMimeType );
+		HttpServer.SendResponse( Response, Client );
+		return;
+	}
+	
+	Http::TResponseProtocol Response( Http::Response_FileNotFound );
+	HttpServer.SendResponse( Response, Client );
+};
+
+
 
 
 int main()
 {
+	//	copy the debug output
+	std::Debug.GetOnFlushEvent().AddListener( [](const std::string& Debug) {	PopLeap::PushDebugLog( Debug );	} );
+	
 	TPopLeapApp App;
 
-	
-	std::cout << "hello!" << std::endl;
+	THttpServer HttpServer( 8080, OnHttpRequest );
+
+	std::Debug << "HTTP Listening on " << HttpServer.GetListeningPort() << std::endl;
 	
 	App.mConsoleApp.WaitForExit();
 
